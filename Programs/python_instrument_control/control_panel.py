e6885f24fc637da3566d796acfa67ce2a2aa9a41
carterfox <102824456+carterfox@users.noreply.github.com>
2025-08-06 11:02:38 -0500

    rmcd updates
    rmcd updates

--- Programs/python_instrument_control/control_panel.py
+++ Programs/python_instrument_control/control_panel.py
@@ -6,13 +6,14 @@ Created on Sun Aug  3 12:32:27 2025
 @author: carterfox
 """
 
-import MultiPyVu as mpv
+# import MultiPyVu as mpv
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from homemade_servers import SSI_OE1022D
-import experiments.RMCD as RMCD
+import SSI_OE1022D
+import RMCD
 from pymeasure.instruments.keithley import Keithley2400,Keithley2450
+from pymeasure.instruments.attocube import ANC300Controller
 
 
 ### functions for getting instruments
@@ -28,10 +29,12 @@ def get_lockin(resource_name='ASRL12::INSTR', R_chan=1, dR_chan=2, num_avgs=150,
     return lockin
 
 def get_opticool(opticool_ip='169.254.170.239', port=5000):
+    import MultiPyVu as mpv
     opticool = mpv.Client(opticool_ip,port)
     current_temp = opticool.get_temperature()
     current_field = opticool.get_field()
     return opticool, current_temp, current_field
+
 
 def get_kiethley2450(resource_name="GPIB0::1::INSTR"):
     keithley2450 = Keithley2450(resource_name)
@@ -42,8 +45,14 @@ def get_kiethley2400(resource_name="GPIB0::16::INSTR"):
     return keithley2400
 
 
+def get_anc300(resource_name='ASRL12::INSTR'):
+    anc300 = ANC300Controller(resource_name, axisnames=['scanx', 'scany','stepperx','steppery'])
+    return anc300
+
+
 
 ### functions for running experiments
+
 def RMCD_experiment(b_start,b_end,b_step):
     
     lockin = get_lockin()
@@ -59,10 +68,9 @@ def RMCD_experiment(b_start,b_end,b_step):
 if __name__ == "__main__":
     
     sample = 'dualgate-s1'
-    data_saving_path = ''
+    # data_saving_path = ''
 
-    
-    rmcd_scan_data = RMCD_experiment(-1,1,0.1)
+    # rmcd_scan_data = RMCD_experiment(-1,1,0.1)
     
     #save data...
     
--- Programs/python_instrument_control/experiments/RMCD.py
+++ Programs/python_instrument_control/experiments/RMCD.py
@@ -34,9 +34,21 @@ def read_lockin_rmcd_data(lockin):
     return R_cur_mean,R_cur_std,theta_R_cur_mean,theta_R_cur_std,dR_cur_mean,dR_cur_std,theta_dR_cur_mean,theta_dR_cur_std
 
 
-def make_saving_file(filename):
+def make_bfield_scan_saving_file(filename):
     
     header = "#B(Oe) R_mean(V) R_std(V) thetaR_mean(deg) thetaR_std(deg) dR_mean(V) dR_std(V) thetadR_mean(deg) thetadR_std(deg)"
+    
+    if not os.path.exists(filename):
+        np.savetxt(filename, [], header=header)
+    else:
+        print('file already exists. making a new one with add on to name')
+        while os.path.exists(filename):            
+            filename = filename.replace(".txt", "_new.txt")
+        np.savetxt(filename, [], header=header)
+        
+def make_mapping_saving_file(filename):
+    
+    header = "#X(V) Y(V) R_mean(V) R_std(V) thetaR_mean(deg) thetaR_std(deg) dR_mean(V) dR_std(V) thetadR_mean(deg) thetadR_std(deg)"
     
     if not os.path.exists(filename):
         np.savetxt(filename, [], header=header)
@@ -56,9 +68,30 @@ def save_rmcd_data_row(data,file_save):
     
 
 def RMCD_bfield_scan(lockin,opticool,bfield_array,file_save):
+    
+    """
+    Performs a magnetic field-dependent RMCD (Reflective Magnetic Circular Dichroism) scan
+    by sweeping through a list of magnetic field values, collecting data at each point using
+    a lock-in amplifier, and saving the results to a file.
+
+    Parameters:
+    ----------
+    lockin : object
+        Lock-in amplifier interface used to collect RMCD data.
+    opticool : object
+        Magnetic field controller (OptiCool) used to set and stabilize the field.
+    bfield_array : list or np.ndarray
+        Array of magnetic field values (in Tesla or Gauss, depending on system units) to scan.
+    file_save : str
+        Path to the file where scan data will be saved.
+
+    Returns:
+    -------
+    None
+    """
 
     delay = 5    
-    make_saving_file(file_save)
+    make_bfield_scan_saving_file(file_save)
         
     for b in bfield_array:
         
@@ -73,4 +106,78 @@ def RMCD_bfield_scan(lockin,opticool,bfield_array,file_save):
     return None
 
 
+def rmcd_mapping(lockin, anc300, x_start, x_end, y_start, y_end, points, returnsteps,file_save):
+    
+    """
+    Performs a raster scan over a 2D grid to collect RMCD (Reflective Magnetic Circular Dichroism) data
+    using a lock-in amplifier and motion controller, and saves the results to a file.
+
+    The scan proceeds line-by-line in the X direction for each Y level, collecting data at each point.
+    After each forward scan line, the instrument performs a stepwise return movement in X (without recording data)
+    to prepare for the next Y level. The collected RMCD values are stored in a 2D NumPy array and saved
+    to a file with corresponding X and Y coordinates.
+
+    Parameters:
+    ----------
+    lockin : object
+        Lock-in amplifier interface used to collect RMCD data.
+    anc300 : object
+        Motion controller interface (not directly used in this function but assumed to control movement).
+    x_start : float
+        Starting X position of the scan.
+    x_end : float
+        Ending X position of the scan.
+    y_start : float
+        Starting Y position of the scan.
+    y_end : float
+        Ending Y position of the scan.
+    points : int
+        Number of scan points along each axis (X and Y).
+    returnsteps : int
+        Number of steps used to return to the starting X position after each scan line.
+        These movements are not recorded in the output array.
+    file_save : str
+        Path to the file where scan data will be saved.
+
+    Returns:
+    -------
+    scan_array : np.ndarray
+        A 2D array of shape (points, points) containing RMCD values at each scanned (X, Y) position.
+        Each value is computed as `data[4] / data[0]` from the lock-in amplifier output.
+    """
+    
+    make_mapping_saving_file(file_save)
+    
+    y_points = points
+    x_points = points
+   
+    stepx = (x_end-x_start)/x_points
+    stepy = (y_end-y_start)/y_points
+
+    scan_array = np.zeros((y_points, x_points))
+    return_step_size = -(x_end - x_start) / returnsteps
+    
+    for j in range(y_points):
+        y = y_start + j * stepy
+
+        # Forward scan
+        for i in range(x_points):
+            lockin.reset_buffer()
+            x = x_start + i * stepx
+            print('mov')
+            data = read_lockin_rmcd_data(lockin)
+            data_row = data.insert(0,y)
+            data_row = data.insert(0,x)
+            save_rmcd_data_row(data_row,file_save)
+            
+            rmcd_value = data[4]/data[0]
+            scan_array[j, i] = rmcd_value
+
+        # Return path (no data stored)
+        for r in range(1, returnsteps + 1):
+            x_back = x_end + r * return_step_size
+            print('move_to(x_back, y)  # Just movement, no data recorded')
+    
+        
+    return scan_array
     
