2ebf20d0f91486b7fb7d960e560485284ab6925f
carterfox <102824456+carterfox@users.noreply.github.com>
2025-10-07 16:24:10 -0500

    raman update

--- python_instrument_control/experiments/raman_basic.py
+++ python_instrument_control/experiments/raman_basic.py
@@ -4,56 +4,53 @@ import matplotlib.pyplot as plt
 from homemade_servers.AndorCameraSpectrometer import AndorCamSpec
 from homemade_servers.ThorlabsKCube import RotationMount
 import toolbelt as tb
+import time
 tb.init_plot_params()
 
-
-def plot_raman(cam_spec, raman_shift, spectrum, raman_range_cm=(100, 3500)):
-    mask = (raman_shift >= raman_range_cm[0]) & (raman_shift <= raman_range_cm[1])
-    raman_filtered = raman_shift[mask]
-    spectrum_filtered = spectrum[mask]
-
-    #plt.figure(figsize=(10, 6))
-    fig, ax = plt.subplots(1,1,figsize=(6,5))
-    
-    ax.plot(raman_filtered, spectrum_filtered, color='b')
-    ax.set_xlabel("Raman Shift (cm⁻¹)")
-    ax.set_ylabel("Intensity (a.u.)")
-    # fig.title(f"Raman Spectrum (Excitation: {cam_spec.excitation_nm} nm)")
-    # plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-
-
-# Measurement
 
 exposure_time = 1
 averages = 10
 angles = np.arange(0,200,20)
-
 
 cam_spec = AndorCamSpec()
 waveplate = RotationMount()
 
 cam_spec.set_accumulations(averages)
 cam_spec.set_exposure(exposure_time)
+raman_shifts = cam_spec.raman_shifts
+raman_range_cm = (10,600)
+mask = (raman_shifts >= raman_range_cm[0]) & (raman_shifts <= raman_range_cm[1])
 
-spectra, raman_shifts, metadatas = []
+
+plt.ion()  # Turn on interactive mode
+fig, ax = plt.subplots()
+line, = ax.plot([],[])
+line.set_xdata(raman_shifts[mask])
+ax.set_title("Live Summed Spectrum")
+ax.set_xlabel("Raman Shift (cm$^{-1}$)"),ax.set_ylabel("Counts")
+ax.grid(True)
+
 
 for angle in angles:
     
     waveplate.move_to(angle)
+    collected_frames = []
     
-    raman_shift, spectrum, metadata = cam_spec.run(subtract_dark=False)
+    while len(collected_frames) < averages:
+        frames = cam_spec.read_multiple_images(timeout=1)
+        if frames: 
+            collected_frames.extend(frames)
+            frames_array = np.array(collected_frames)    
+            summed_spectrum = np.sum(frames_array, axis=0)
+            line.set_ydata(summed_spectrum[mask])
+            ax.relim()
+            ax.autoscale_view()
+        else: 
+            time.sleep(0.1) # no new frames yet, wait a bit
     
-    spectra.append(spectrum)
-    raman_shifts.append(raman_shift)
-    metadatas.append(metadata)
 
-
-
-# Plotting
-plot_raman(cam_spec, raman_shift, spectrum, raman_range_cm=(100, 3000), metadata=metadata)
-
+plt.ioff()
+plt.show()
 # Cleanup
+# cam_spec.cam.abort_acquisition()
 # cam_spec.close()
--- python_instrument_control/homemade_servers/AndorCameraSpectrometer.py
+++ python_instrument_control/homemade_servers/AndorCameraSpectrometer.py
@@ -6,37 +6,37 @@ import logging
 import time
 
 class AndorCamSpec:
-    def __init__(self, exposure=1.0, temperature=-85, n_avg=3, excitation_nm=632.8):
+    def __init__(self, exposure=1.0, temperature=-85, accumulations=3, excitation_nm=632.8):
         pll.par["devices/dlls/andor_sdk2"] = 'C:/Program Files/Andor SDK'
         pll.par["devices/dlls/andor_shamrock"] = 'C:/Program Files/Andor SDK/Shamrock64'
-        self.cam = Andor.AndorSDK2Camera(temperature=temperature,fan_mode='full',)
+        self.cam = Andor.AndorSDK2Camera(temperature=temperature,fan_mode='full')
         self.spec = Andor.ShamrockSpectrograph()
+
         self.excitation_nm = excitation_nm
-        self.n_avg = self.set_accumulations(n_avg)
         self.central_wavelength = 550
         self.grating = 3
         self.configure_spectrometer(grating=self.grating, central_wl=self.central_wavelength)
+        self.accumulations = accumulations
+        self.set_exposure(exposure)
 
-        
         # Camera setup
         # we want the cooler to stay on when we do measurements and only turn off after 
         # all the measurements are done and we decide it is time to turn it off.
         # To accomplish that, the control panel has 'open_instruments' and leave_servers_open' booleans
         # that ask if the servers should be left opened and if they should be left open.
-        
         self.cam.set_cooler(True)
         self.cam.set_temperature(temperature)
-        self.set_exposure(exposure)
+        
         self.cam.set_read_mode('fvb')
-        self.cam.set_acquisition_mode('accum')
+        self.cam.set_acquisition_mode("kinetic series")
+        self.cam.set_number_kinetic_series_images(self.accumulations)
         logging.info('Connected to Andor CamSpec. Temp = ',self.cam.get_temperature())
 
     def close(self):
         self.cam.set_temperature(0) 
         while True:
             temp = self.cam.get_temperature()
-            if temp >= -50:
-                break
+            if temp >= -50: break
             time.sleep(2)
         self.cam.set_cooler(False)
         self.cam.close()
@@ -46,51 +46,40 @@ class AndorCamSpec:
     def set_exposure(self,exposure_time):
         self.cam.set_exposure(exposure_time)
         
-    def set_accumulations(self,accumulations):
-        self.n_avg = accumulations
+    def read_multiple_images(self,rng=None):
+        self.cam.read_multiple_images(rng)
         
+    def set_accumulations(self,accumulations):
+        self.accumulations = accumulations
+        self.cam.set_number_kinetic_series_images(self.accumulations)
+    
     def configure_spectrometer(self, grating=3, central_wl=550):
         self.spec.set_grating(grating)
         self.spec.set_central_wavelength(central_wl)
+        self.spec.setup_pixels_from_camera(self.cam)
+        self.wavelengths = self.spec.get_calibration()
+        self.raman_shifts = self.wavelength_to_raman_shift(self.wavelengths)
         # self.spec.set_slit_width(slit_width)
-
-    def acquire_spectrum(self):
-        spectra = [self.cam.get_spectrum() for _ in range(self.n_avg)]
-        return np.mean(spectra, axis=0)
-
-    def acquire_dark(self):
-        input("Close shutter or block laser, then press Enter...")
-        dark_frames = [self.cam.get_spectrum() for _ in range(self.n_avg)]
-        return np.mean(dark_frames, axis=0)
-
+    
     def wavelength_to_raman_shift(self, wavelengths_nm):
         excitation_cm = 1e7 / self.excitation_nm
         measured_cm = 1e7 / np.array(wavelengths_nm)
         return excitation_cm - measured_cm
 
+    def get_metadata(self):
+        metadata = { "grating": self.spec.get_grating(), 
+                    "central_wavelength": self.spec.get_central_wavelength(),
+                    "slit_width": self.spec.get_slit_width(),
+                    "temperature": self.cam.get_temperature(),
+                    "exposure_time": self.cam.get_exposure_time(),
+                    "kinetic mode params (num_cycle, cycle_time, num_acc, cycle_time_acc, num_prescan)": self.cam.get_kinetic_mode_parameters()
+                    }
+        return metadata
+        
     # def remove_spikes(self,data):
         #insert later... 
         # return filtered_data
 
-    def run(self, subtract_dark=False):
-        wavelengths = self.cam.get_wavelengths()
-        spectrum = self.acquire_spectrum()
-
-        if subtract_dark:
-            dark = self.acquire_dark()
-            spectrum -= dark
-
-        raman_shift = self.wavelength_to_raman_shift(wavelengths)
-
-        metadata = {
-            "grating": self.spec.get_grating(),
-            "central_wavelength": self.spec.get_central_wavelength(),
-            "slit_width": self.spec.get_slit_width(),
-            "exposure_time": self.cam.get_exposure_time(),
-            "temperature": self.cam.get_temperature()
-        }
-
-        return raman_shift, spectrum, metadata
     
     
     
