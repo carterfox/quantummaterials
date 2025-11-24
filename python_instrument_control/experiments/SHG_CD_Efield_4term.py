b6d183390000574804fd7351f0351f9e70fa5922
carterfox <102824456+carterfox@users.noreply.github.com>
2025-11-24 10:30:28 -0600

    minor

--- python_instrument_control/experiments/SHG_CD_Efield_4term.py
+++ python_instrument_control/experiments/SHG_CD_Efield_4term.py
@@ -18,6 +18,7 @@ from homemade_servers.H11890PMT import HamamatsuH11890
 from homemade_servers.ThorlabsKCube import RotationMount
 from homemade_servers.KeithleySourceMeter import KeithleySourceMeter
 from devices.transport import FourTerminal
+import math
 
 def main(sample: FourTerminal, keithley_x: KeithleySourceMeter, keithley_y: KeithleySourceMeter, pmt: HamamatsuH11890, qwp_rotstage: RotationMount,
          qwp_angles=(0,90),gate_time_ms=200, num_gates=10, laser_power=0,Ex_array=np.array([]),Ey_array=np.array([]),file_save='test.txt'):
@@ -29,7 +30,7 @@ def main(sample: FourTerminal, keithley_x: KeithleySourceMeter, keithley_y: Keit
     #### turn on PMT, set keithleys, then initiate a bunch of litsts. angle_ind controls qwp setting (C1 vs C2)
     # pmt.set_hv(on=True)   
     set_keithleys(keithley_x,keithley_y)
-    Ex_list, Ey_list, SHG_total_list, SHG_CD_list, SHG_C_vals, SHG_C_stds, angle_ind = [],[],[],[],  [[],[]] , [[],[]], 0
+    Ex_list, Ey_list, SHG_total_list, SHG_CD_list, SHG_CD_std_list, SHG_C_vals, SHG_C_stds, angle_ind = [],[],[],[],[],  [[],[]] , [[],[]], 0
     
     for (Ex,Ey) in zip(Ex_array,Ey_array):
         
@@ -46,10 +47,10 @@ def main(sample: FourTerminal, keithley_x: KeithleySourceMeter, keithley_y: Keit
             SHG_C_vals[angle_ind].append(np.mean(data)), SHG_C_stds[angle_ind].append(np.mean(np.std(data)/np.sqrt(num_gates)))
 
         #### compute total SGH and SHG-CD. plot and save data    
-        SHG_C1, SHG_C1_std, SHG_C2, SHG_C2_std, SHG_total, SHG_CD = get_SHG_vals(SHG_C_vals,SHG_C_stds)
-        SHG_total_list.append(SHG_total), SHG_CD_list.append(SHG_CD), Ex_list.append(Ex), Ey_list.append(Ey)
-        update_plot(fig,ax1,ax2,line1,line2,Ex_list,Ey_list,SHG_total_list,SHG_CD_list)
-        update_saved_data(file_path,Vx,Vy,SHG_C1,SHG_C1_std,SHG_C2,SHG_C2_std,Ix_meas,Iy_meas)
+        SHG_C1, SHG_C1_std, SHG_C2, SHG_C2_std, SHG_total, SHG_CD, SHG_CD_std = get_SHG_vals(SHG_C_vals,SHG_C_stds)
+        SHG_total_list.append(SHG_total), SHG_CD_list.append(SHG_CD), SHG_CD_std_list.append(SHG_CD_std), Ex_list.append(Ex), Ey_list.append(Ey)
+        update_plot(fig,ax1,ax2,line1,line2,Ex_list,Ey_list,SHG_total_list,SHG_CD_list,SHG_CD_std_list)
+        update_saved_data(file_path,Vx,Vy,SHG_C1,SHG_C1_std,SHG_C2,SHG_C2_std,SHG_CD,SHG_CD_std,Ix_meas,Iy_meas)
    
     ### turn off PMT hv and interactive plotting
     # pmt.set_hv(on=False)
@@ -61,7 +62,7 @@ def main(sample: FourTerminal, keithley_x: KeithleySourceMeter, keithley_y: Keit
     return SHG_total_list,SHG_CD_list
     
 
-def update_plot(fig,ax1,ax2,line1,line2,Ex_list,Ey_list,SHG_total_list,SHG_CD_list):
+def update_plot(fig,ax1,ax2,line1,line2,Ex_list,Ey_list,SHG_total_list,SHG_CD_list,SHG_CD_std_list):
     #currently just using Ex
     line1.set_data(Ex_list,SHG_total_list)
     line2.set_data(Ex_list,SHG_CD_list)
@@ -101,12 +102,12 @@ def make_data_file(sample,qwp_angles,laser_power,gate_time_ms,num_gates,file_sav
         f.write(f"# Laser Power: {laser_power} mW\n")
         f.write(f"# Gate Time: {gate_time_ms} ms\n")
         f.write(f"# Num Gates: {num_gates}\n")
-        f.write("# Vx (V) \t Vy (V) \t C1CountsMean \t C1CountsStd \t C2CountsMean \t C2CountsStd \t Ix (nA) \t Iy (nA) \n")
+        f.write("# Vx (V) \t Vy (V) \t C1CountsMean \t C1CountsStd \t C2CountsMean \t C2CountsStd \t SHG_CD \t SHG_CD_std \t Ix (nA) \t Iy (nA) \n")
     return file_path
 
-def update_saved_data(file_path,Vx,Vy,C1_mean,C1_std,C2_mean,C2_std,Ix,Iy):
+def update_saved_data(file_path,Vx,Vy,C1_mean,C1_std,C2_mean,C2_std,CD,CD_std,Ix,Iy):
     with open(file_path, 'a') as f:
-        data_save = [Vx,Vy,C1_mean,C1_std,C2_mean,C2_std,Ix,Iy]
+        data_save = [Vx,Vy,C1_mean,C1_std,C2_mean,C2_std,CD,CD_std,Ix,Iy]
         f.write(' '.join(f"{d:.3f}" for d in data_save) + '\n') 
 
 def update_rotation_stage(qwp_rotstage: RotationMount,qwp_angle):
@@ -136,7 +137,8 @@ def get_SHG_vals(SHG_C_vals,SHG_C_stds):
     SHG_total = SHG_C1 + SHG_C2
     SHG_diff = SHG_C1 - SHG_C2
     SHG_CD = SHG_diff/SHG_total*100
-    return SHG_C1, SHG_C1_std, SHG_C2, SHG_C2_std, SHG_total, SHG_CD
+    SHG_CD_std = get_SGH_CD_std(SHG_C1, SHG_C2, SHG_C1_std, SHG_C2_std)
+    return SHG_C1, SHG_C1_std, SHG_C2, SHG_C2_std, SHG_total, SHG_CD, SHG_CD_std
 
 def set_keithleys(keithley_x,keithley_y):
     if keithley_x != None:
@@ -145,3 +147,31 @@ def set_keithleys(keithley_x,keithley_y):
     if keithley_y != None:
         keithley_y.enable_source()
         keithley_y.apply_voltage()
+
+def get_SGH_CD_std(x, y, sigma_x, sigma_y):
+    numerator = math.sqrt((y**2) * (sigma_x**2) + (x**2) * (sigma_y**2))
+    denominator = (x + y)**2
+    return 100 * 2 * numerator / denominator
+
+if __name__ == "__main__":
+
+    tb.init_plot_params()
+    # path_d3 = 'D:/LabData/XiaoWang_Group_data_2024on/StackingTransitions/CrI3/round7/d3/RMCD/Esweep/'
+    path_d1 = '/Users/carterfox/My Drive (cdfox@wisc.edu)/StackingTransitions/NbOI2/Lvgroup/Efield-samples-for-optics/90deg_3L3L_4term_S1/SHG-CD-Efield/'
+    sample = FourTerminal('NbOI290deg4termS1', 5, path_d1)
+    w = sample.channel_width
+    
+    file_path = path_d1+'test.txt'
+    
+    data = np.loadtxt(file_path,comments='#')
+    Vx,Vy,SHG_C1,SHG_C1_std,SHG_C2,SHG_C2_std,SHG_CD,SHG_CD_std,Ix,Iy = data[:,0],data[:,1],np.array(data[:,2]),np.array(data[:,3]),np.array(data[:,4]),np.array(data[:,5]),np.array(data[:,6]),np.array(data[:,7]),data[:,8],data[:,9]
+    Ex_list, Ey_list = Vx/w, Vy/w
+    SHG_total = SHG_C1 + SHG_C2
+    
+    fig,ax1,line1,ax2,line2 = init_main_plot()
+    update_plot(fig,ax1,ax2,line1,line2,Ex_list,Ey_list,SHG_total,SHG_CD)
+    
+    
+    
+    
+
